2db67a20aa17780bab71f5831d5c0caf213a8cc2
Rtogoev <rtogoev@example.com>
2019-04-16 21:31:54 +0300

    Начало процесса. Создан собственный класс на основе массива, но без массива. Создан тест.

---
+++ hw02-DIYarrayList/src/test/java/com/DIYarrayListTest.java
@@ -0,0 +1,122 @@
+package com;
+
+
+import org.junit.jupiter.api.Test;
+
+class DIYarrayListTest {
+
+    @Test
+    void size() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void isEmpty() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void contains() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void toArray() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void toArray1() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void add() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void remove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void containsAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void addAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void addAll1() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void removeAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void retainAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void get() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void set() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void add1() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void remove1() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void indexOf() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void lastIndexOf() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void listIterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void listIterator1() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    void subList() {
+        throw new UnsupportedOperationException();
+    }
+}
