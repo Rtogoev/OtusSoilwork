4ad63478c5762ad8a916560c9d9589b7f9dc9d7b
rtogoev <rtogoev@megalabs.ru>
2019-09-02 17:04:30 +0300

    Теперь все джарники запускаются через MessageServer. Ошибка при запуске из-за занятого порта. Похоже запуск должен быть последовательный.

--- hw16-MessageServer/MessageServer/src/main/java/ru/otus/homework/service/BackendRunnerService.java
+++ hw16-MessageServer/MessageServer/src/main/java/ru/otus/homework/service/BackendRunnerService.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 @Service
 public class BackendRunnerService {
 
-    private static final String BACKEND_RUN_COMMAND = "java -jar Backend-1.0-SNAPSHOT.jar";
+    private static final String BACKEND_RUN_COMMAND = "java -jar hw16-MessageServer/Backend/target/Backend-1.0-SNAPSHOT.jar";
     private final RunnerService runnerService;
     @Value("${backendInstanseCount}")
     private int backendInstanseCount;
@@ -20,6 +20,6 @@ public class BackendRunnerService {
 
     @PostConstruct
     void init() throws IOException, InterruptedException {
-        runnerService.run(BACKEND_RUN_COMMAND,backendInstanseCount);
+        runnerService.run(BACKEND_RUN_COMMAND, backendInstanseCount);
     }
 }
--- hw16-MessageServer/MessageServer/src/main/java/ru/otus/homework/service/RunnerService.java
+++ hw16-MessageServer/MessageServer/src/main/java/ru/otus/homework/service/RunnerService.java
@@ -2,21 +2,39 @@ package ru.otus.homework.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Service
 public class RunnerService {
 
     private void runInstance(String command) throws IOException, InterruptedException {
         ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+        processBuilder.redirectErrorStream(true);
         Process process = processBuilder.start();
-        while(process.isAlive()){
-            Thread.sleep(1000);
-        }
+        new Thread(
+                () -> {
+                    try {
+                        while (process.isAlive()) {
+                            Thread.sleep(1000);
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                            String line = reader.readLine();
+                            while ((line != null) || process.isAlive() || line.length() == 0 || line.equals("")) {
+                                System.out.println(line);
+
+                                line = reader.readLine();
+                            }
+                        }
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        ).start();
     }
 
-    public void run(String command,int instanceCount) throws IOException, InterruptedException {
-        for (int i=0;i<instanceCount;i++){
+    public void run(String command, int instanceCount) throws IOException, InterruptedException {
+        for (int i = 0; i < instanceCount; i++) {
             runInstance(command);
         }
     }
